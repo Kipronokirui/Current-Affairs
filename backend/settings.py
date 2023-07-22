afcfc8487c8c74b9e0886233a6c64f5d8a866195
Kipronokirui <victorkipronokirui2@gmail.com>
2023-07-23 00:11:57 +0300

    deploy update

--- backend/settings.py
+++ backend/settings.py
@@ -115,6 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 CKEDITOR_UPLOAD_PATH = "ckuploads/"
 
 # Default primary key field type
